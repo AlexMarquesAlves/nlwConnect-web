074acb2f211775ab020a48a7e5643664ceff8d9d
¡Duque <atanaelleonardo@gmail.com>
2025-02-20 18:30:01 -0300

    feat: update layout and styling

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -8,8 +8,23 @@ export default function InvitePage() {
         <Image src={logo} alt="DevStage Logotipo" width={108.5} height={30} />
 
         <div className="space-y-2">
-          <h1>Inscrição confirmada!</h1>
-          <p>Para entrar no evento, acesse o link enviado para o seu e-mail.</p>
+          <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
+            Inscrição confirmada!
+          </h1>
+          <p className="text-gray-300">
+            Para entrar no evento, acesse o link enviado para o seu e-mail.
+          </p>
+        </div>
+
+        <div className="space-y-6">
+          <div className="space-y-3">
+            <h2>Indique e ganhe</h2>
+            <p>
+              Convide mais pessoas para o evento e concorra a prêmios
+              exclusivos! É só compartilhar o link abaixo e acompanhar as
+              inscrições:
+            </p>
+          </div>
         </div>
       </div>
     </div>
