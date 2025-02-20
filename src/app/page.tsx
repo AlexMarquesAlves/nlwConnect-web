66482546164ff3edaadd1456fb79bb158ccf19f2
¡Duque <atanaelleonardo@gmail.com>
2025-02-20 17:39:18 -0300

    feat: add Radio component and modified layout for "Sobre o evento" section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Radio } from 'lucide-react'
 import Image from 'next/image'
 import logo from '../assets/logo.svg'
 
@@ -13,12 +14,18 @@ export default function Home() {
           </h1>
         </div>
         <div className="flex gap-5 items-stretch flex-col md:flex-row">
+          {/* Sobre o Evento */}
           <div className="flex-1 bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6">
-            <div>
-              <h2>Sobre o evento</h2>
-              <span>AO VIVO</span>
+            <div className="flex items-center justify-between">
+              <h2 className="font-heading font-semibold text-gray-200 text-xl">
+                Sobre o evento
+              </h2>
+              <span className="text-purple font-semibold text-xs flex items-center gap-2">
+                <Radio className="size-5" />
+                AO VIVO
+              </span>
             </div>
-            <p>
+            <p className="text-gray-300 leading-relaxed text-sm md:text-base">
               Um evento feito por e para pessoas desenvolvedoras apaixonadas por
               criar soluções inovadoras e compartilhar conhecimento. Vamos
               mergulhar nas tendências mais recentes em desenvolvimento de
@@ -29,7 +36,8 @@ export default function Home() {
               Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
             </p>
           </div>
-          <form action="">Form</form>
+          {/* Inscrição */}
+          <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"></form>
         </div>
       </div>
     </main>
