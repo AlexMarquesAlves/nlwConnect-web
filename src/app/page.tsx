70143961f34a5669fd063855377afe00774b488a
¡Duque <atanaelleonardo@gmail.com>
2025-02-20 16:59:55 -0300

    removed unused React components and exports and add some project assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-import { ArrowRight, Copy, Mail } from 'lucide-react'
-import {
-  Button,
-  IconButton,
-  InputField,
-  InputIcon,
-  InputRoot,
-} from './components'
-
 export default function Home() {
-  return (
-    <main>
-      <Button type="submit">
-        Enviar <ArrowRight />
-      </Button>
-
-      <IconButton>
-        <Copy />
-      </IconButton>
-
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail className="size-5" />
-          </InputIcon>
-          <InputField />
-        </InputRoot>
-      </div>
-    </main>
-  )
+  return <main>{''}</main>
 }
