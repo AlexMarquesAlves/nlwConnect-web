581ea9c46d456408f01791cde898b03f706f8a4f
¡Duque <atanaelleonardo@gmail.com>
2025-02-20 18:19:23 -0300

    feat: refactor Invite page: add logo, layout and content

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,9 +1,17 @@
-function Invite() {
+import Image from 'next/image'
+import logo from '../../assets/logo.svg'
+
+export default function InvitePage() {
   return (
-    <>
-      <h1>Invite</h1>
-    </>
+    <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
+      <div className="flex flex-col gap-10 w-full max-w-[550px]">
+        <Image src={logo} alt="DevStage Logotipo" width={108.5} height={30} />
+
+        <div className="space-y-2">
+          <h1>Inscrição confirmada!</h1>
+          <p>Para entrar no evento, acesse o link enviado para o seu e-mail.</p>
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default Invite
