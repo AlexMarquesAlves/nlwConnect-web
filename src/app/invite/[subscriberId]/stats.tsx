700fead155206a221983bc4abc197a1e486a180f
¡Duque <atanaelleonardo@gmail.com>
2025-02-23 20:44:14 -0300

    feat: add getSubscriberInviteCount and getSubscriberRankingPosition API calls to stats component

--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -1,4 +1,8 @@
-import { getSubscriberInviteClicks } from '@/http/api'
+import {
+  getSubscriberInviteClicks,
+  getSubscriberInviteCount,
+  getSubscriberRankingPosition,
+} from '@/http/api'
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
 
 interface StatsProps {
@@ -6,6 +10,9 @@ interface StatsProps {
 }
 export async function Stats({ subscriberId }: StatsProps) {
   const { count: accessCount } = await getSubscriberInviteClicks(subscriberId)
+  const { count: inviteCount } = await getSubscriberInviteCount(subscriberId)
+  const { position: rankingPosition } =
+    await getSubscriberRankingPosition(subscriberId)
 
   return (
     <div className="grid gap-3 md:grid-cols-3">
@@ -22,7 +29,7 @@ export async function Stats({ subscriberId }: StatsProps) {
 
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          1042
+          {inviteCount}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Inscrições Feitas
@@ -33,7 +40,7 @@ export async function Stats({ subscriberId }: StatsProps) {
 
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          3°
+          {rankingPosition ? `${rankingPosition}°` : '-'}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Posição no ranking
