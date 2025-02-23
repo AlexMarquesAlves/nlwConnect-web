80774cfed2c5f9f93ce8cc738d82672a8822e980
¡Duque <atanaelleonardo@gmail.com>
2025-02-23 18:12:38 -0300

    feat: add react-hook-form and form submission functionality to subscription form

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -1,14 +1,25 @@
+'use client'
+
 import { Button, InputField, InputIcon, InputRoot } from '@/components'
 import { ArrowRight, Mail, User } from 'lucide-react'
 import type { ReactNode } from 'react'
+import { useForm } from 'react-hook-form'
 
 interface SubscriptionFormProps {
   children?: ReactNode
 }
 
 export function SubscriptionForm() {
+  const { register, handleSubmit } = useForm()
+  function onSubscribe(data: unknown) {
+    console.log(data)
+  }
+
   return (
-    <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
+    <form
+      onSubmit={handleSubmit(onSubscribe)}
+      className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
+    >
       <h2 className="font-heading font-semibold text-gray-200 text-xl">
         Inscrição
       </h2>
@@ -18,14 +29,22 @@ export function SubscriptionForm() {
           <InputIcon>
             <User />
           </InputIcon>
-          <InputField type="text" placeholder="Nome completo" />
+          <InputField
+            type="text"
+            placeholder="Nome completo"
+            {...register('name')}
+          />
         </InputRoot>
 
         <InputRoot>
           <InputIcon>
             <Mail />
           </InputIcon>
-          <InputField type="email" placeholder="E-mail" />
+          <InputField
+            type="email"
+            placeholder="Seu e-mail"
+            {...register('email')}
+          />
         </InputRoot>
       </div>
       <Button>
